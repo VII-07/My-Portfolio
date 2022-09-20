0dcae264733b430f1dacb516182e2a1cb88437e2
Seventh <masterb21052017@gmail.com>
2022-09-20 22:37:33 +0300

    3 section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 // Import our custom CSS
 import './styles/main.scss';
-
 // Import all of Bootstrap's JS
 import 'bootstrap';
 
+mail.addEventListener('click', () => {
+    functionCopy(mail);
+})
 
+function functionCopy(elem){
+    navigator.clipboard.writeText(elem.innerHTML)
+    .then(() => {
+        successMessage.classList.add('active');
+        setTimeout(() => successMessage.classList.remove('active'), 2500);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+}
